b850e89a20000fdc3cc332934b27f434b480d494
rikardro <93663695+rikardro@users.noreply.github.com>
2024-03-08 14:57:51 +0100

    Use case "see all my images" test passes

--- client/src/components/home_page/Grid/Grid.test.tsx
+++ client/src/components/home_page/Grid/Grid.test.tsx
@@ -9,18 +9,6 @@ jest.mock('react-router-dom', () => ({
   useNavigate: jest.fn(), // Mocking useNavigate as a jest.fn()
 }));
 
-// Mock GridImg
-/* jest.mock('./GridImg/GridImg', () => ({
-  __esModule: true,
-  default: jest.fn(() => null), // Mock GridImg as a functional component that renders nothing
-})); */
-
-jest.mock("./GridImg/GridImg", () => {
-  return ({ image }: { image: Image }) => (
-    <div data-testid="grid-img">{image.path}</div>
-  );
-});
-
 // Mock Image data
 const images: Image[] = Array.from({ length: 3 }, (_, i) => ({
   id: i + 1,
@@ -49,50 +37,3 @@ describe('Grid component', () => {
     expect(noImagesDisplay).toBeInTheDocument();
   });
 });
-
-
-//TODO: change this later!
-/* jest.mock("./GridImg", () => {
-  return ({ image }: { image: Image }) => (
-    <div data-testid={`grid-img-${image.id}`}>{image.path}</div>
-  );
-});
-
-describe("Grid component", () => {
-  it("renders correctly with no images", () => {
-    const images: Image[] = [];
-    render(<Grid images={images} callback={() => {}}/>);
-    const noImagesMessage = screen.getByTestId("no-images-message");
-    expect(noImagesMessage).toBeInTheDocument();
-  });
-
-  it("always renders three columns if images not empty", () => {
-    // Create an array of 3 images
-    const images: Image[] = Array.from({ length: 3 }, (_, i) => ({
-      id: i + 1,
-      filename: `${i + 1}.png`,
-      path: `https://example.com/${i + 1}.png`,
-      uploadDate: new Date(),
-    }));
-    const { container } = render(<Grid images={images} callback={() => {}}/>);
-    const columns = container.querySelectorAll(".col-md-4"); // Assuming you're using Bootstrap or similar for columns
-    expect(columns.length).toBe(3);
-  });
-
-  [1, 2, 4, 5].forEach((count) => {
-    it(`distributes ${count} image(s) evenly across columns`, () => {
-      const images: Image[] = Array.from({ length: count }, (_, i) => ({
-        id: i + 1,
-        filename: `${i + 1}.png`,
-        path: `https://example.com/${i + 1}.png`,
-        uploadDate: new Date(),
-      }));
-
-      render(<Grid images={images} callback={() => {}}/>);
-      images.forEach((image) => {
-        const renderedImage = screen.getByTestId(`grid-img-${image.id}`);
-        expect(renderedImage).toHaveTextContent(image.path);
-      });
-    });
-  });
-}); */
