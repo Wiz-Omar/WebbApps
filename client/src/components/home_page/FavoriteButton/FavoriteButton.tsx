6c8e6ef7fb84ce33b1300d1d774103fc6e76c8d5
harryden <harry.denell@gmail.com>
2024-03-08 11:56:58 +0100

    Update FavoriteButton.tsx

--- client/src/components/home_page/FavoriteButton/FavoriteButton.tsx
+++ client/src/components/home_page/FavoriteButton/FavoriteButton.tsx
@@ -7,6 +7,16 @@ interface FavoriteButtonProps {
   callback: () => void;
 }
 
+/**
+ * A button that allows the user to like or unlike an image. The button displays a filled heart icon if the image is
+ * liked, and an empty heart icon if the image is not liked.
+ * 
+ * Props:
+ * - `isLiked` (boolean): A boolean value that indicates whether the image is liked.
+ * - `callback` (function): A callback function that is called when the button is clicked. It should handle the like/unlike
+ *  operation and update the parent component's state.
+ * 
+ */
 const FavoriteButton = ({ isLiked, callback }: FavoriteButtonProps) => {
   return (
   <div className="button" onClick={callback}>
