21e913fff8d25a3515e15f912168f9b67d5ca0c9
Omar <oyounes57@gmail.com>
2024-03-09 15:30:05 +0100

    fixed the stacking of navbar elements on medium-small screens

--- Client/src/components/home_page/Navbar/HomeNavbar.tsx
+++ Client/src/components/home_page/Navbar/HomeNavbar.tsx
@@ -22,17 +22,17 @@ interface NavbarProps {
 function Navbar({ callback }: NavbarProps) {
   return (
     <Container fluid className="navbar-light sticky-top rounded bg-light">
-      <Row>
-        <Col md={2} className="d-flex justify-content-start mb-2 align-items-center">
+      <Row className="mx-2">
+        <Col xl={2} className="d-flex justify-content-center mb-2 align-items-center">
           <a className="navbar-brand" href="/">
             <img src={logo} alt="Logo" className="logo-img"></img>
             <span className="logo-text">PicPics</span>
           </a>
         </Col>
-        <Col md={6} className="d-flex justify-content-center my-2 align-items-center">
+        <Col xl={6} className="d-flex justify-content-center my-2 align-items-center">
           <SearchBar/>
         </Col>
-        <Col md={4} className="d-flex justify-content-center mb-2 align-items-center">
+        <Col xl={4} className="d-flex justify-content-center mb-2 align-items-center">
           <SortDropdown callback={callback} />
           <LikedToggle onToggle={callback} />
           <UploadButton callback={callback} />
