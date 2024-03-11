0b7fa2d18c195cb99ce77c81b7980f7d93d1d171
harryden <harry.denell@gmail.com>
2024-03-11 12:47:17 +0100

    update resizing

--- client/src/components/login_page/LoginPage.tsx
+++ client/src/components/login_page/LoginPage.tsx
@@ -22,16 +22,15 @@ interface CombinedPageProps {
  * to authenticate themselves. Upon successful login, the user is redirected to the home page.
  * If the user is already logged in (session check is positive), they are immediately redirected
  * to the home page, bypassing the login form.
- * 
+ *
  * The component also provides a button to navigate the registration page for users who do not have an account.
- * 
+ *
  * Props:
  *  - setDisplay: Function - A callback function that sets the current display component of the app.
  *    It is used to navigate between different parts of the application, such as from the login page to
  *    the home page or the registration page.
  */
 const LoginPage: React.FC<CombinedPageProps> = ({ setDisplay }) => {
-
   // Use the custom hook to check if the user is already authenticated
   const { isLoading, isAuthenticated } = useCheckUserAuthentication();
 
@@ -57,13 +56,19 @@ const LoginPage: React.FC<CombinedPageProps> = ({ setDisplay }) => {
       ) : (
         <Container className="login-container">
           <Row className="justify-content-md-center">
-            <Col md={6}>
+            <Col xs={12} sm={10} md={8} lg={6}>
               <Logo />
               <h2 className="text-center mb-4">{LOGIN_PROMPT}</h2>
-              <LoginForm onLoginSuccess={handleLoginSuccess} onLoginError={handleLoginError} />
+              <LoginForm
+                onLoginSuccess={handleLoginSuccess}
+                onLoginError={handleLoginError}
+              />
               <div className="text-center mt-4">
                 <p>Don’t have an account?</p>
-                <Button variant="outline-primary" onClick={() => setDisplay(AppDisplay.REGISTER_PAGE)}>
+                <Button
+                  variant="outline-primary"
+                  onClick={() => setDisplay(AppDisplay.REGISTER_PAGE)}
+                >
                   SIGN UP
                 </Button>
               </div>
--- client/src/components/login_page/RegisterPage.tsx
+++ client/src/components/login_page/RegisterPage.tsx
@@ -1,12 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { AppDisplay } from "../../App";
-import {
-  Container,
-  Row,
-  Col,
-  Button,
-} from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import "./LoginPage.css"; // Reuse styles from LoginPage
 
 import { RegisterForm } from "./RegisterForm";
@@ -14,7 +9,8 @@ import { Logo } from "./Logo";
 
 axios.defaults.withCredentials = true;
 
-const REGISTER_PROMPT = "Don't have an account? Register to start using PicPics";
+const REGISTER_PROMPT =
+  "Don't have an account? Register to start using PicPics";
 
 interface RegisterPageProps {
   setDisplay: (display: AppDisplay) => void;
@@ -23,16 +19,15 @@ interface RegisterPageProps {
 /**
  * A component that renders the registration page. This page includes a registration form for new users
  * to create an account. Upon successful registration, the user is redirected to the login page.
- * 
+ *
  * The component also provides a button to navigate to the login page for users who already have an account.
- * 
+ *
  * Props:
  *  - setDisplay: Function - A callback function that sets the current display component of the app.
  *    It is used to navigate between different parts of the application, such as from the registration page to
  *    the login page or the home page.
  */
 const RegisterPage: React.FC<RegisterPageProps> = ({ setDisplay }) => {
-
   // Handle successful registration by redirecting to the login page
   const handleRegisterSuccess = () => {
     setDisplay(AppDisplay.LOGIN_PAGE);
@@ -47,13 +42,19 @@ const RegisterPage: React.FC<RegisterPageProps> = ({ setDisplay }) => {
     <div>
       <Container className="register-container">
         <Row className="justify-content-md-center">
-          <Col md={6}>
+          <Col xs={12} sm={10} md={8} lg={6}>
             <Logo />
             <h2 className="text-center mb-4">{REGISTER_PROMPT}</h2>
-            <RegisterForm onRegisterSuccess={handleRegisterSuccess} onRegisterError={handleRegisterError} />
+            <RegisterForm
+              onRegisterSuccess={handleRegisterSuccess}
+              onRegisterError={handleRegisterError}
+            />
             <div className="text-center mt-4">
               <p>Already have an account?</p>
-              <Button variant="outline-primary" onClick={() => setDisplay(AppDisplay.LOGIN_PAGE)}>
+              <Button
+                variant="outline-primary"
+                onClick={() => setDisplay(AppDisplay.LOGIN_PAGE)}
+              >
                 LOG IN
               </Button>
             </div>
@@ -62,6 +63,6 @@ const RegisterPage: React.FC<RegisterPageProps> = ({ setDisplay }) => {
       </Container>
     </div>
   );
-}
+};
 
 export default RegisterPage;
